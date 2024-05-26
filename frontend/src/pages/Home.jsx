ef11c22bc7bb8e5bea999dc148d452cf85f6a8a5
lawrence2004 <138093175+lawrence2004@users.noreply.github.com>
2024-05-26 16:41:01 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
   useEffect(() => {
     setLoading(true);
     axios
-      .get("http://localhost:3001/books")
+      .get("https://book-store-mern-eta.vercel.app/books")
       .then((response) => {
         setBooks(response.data.data);
         setLoading(false);
