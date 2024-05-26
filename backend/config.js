efaed5dc5b23cdeaefb125249362299c57424ee4
lawrence2004 <138093175+lawrence2004@users.noreply.github.com>
2024-05-26 16:27:46 +0530

    Create config.js

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 3001;
+
+export const MongoDBURL =
+  "mongodb+srv://lawrence18:Lawrence2004@mern-bookstore.pg0l5ix.mongodb.net/?retryWrites=true&w=majority&appName=MERN-BookStore";
