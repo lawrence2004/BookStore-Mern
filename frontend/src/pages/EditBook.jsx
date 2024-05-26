c700d686bb3c1e6bf29e63a91543ef8db083eba7
lawrence2004 <138093175+lawrence2004@users.noreply.github.com>
2024-05-26 16:40:42 +0530

    Update EditBook.jsx

--- frontend/src/pages/EditBook.jsx
+++ frontend/src/pages/EditBook.jsx
@@ -18,7 +18,7 @@ const EditBook = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get(`http://localhost:3001/books/${id}`)
+    axios.get(`https://book-store-mern-eta.vercel.app/books/${id}`)
     .then(response => {
        setTitle(response.data.title);
        setAuthor(response.data.author);
@@ -39,7 +39,7 @@ const EditBook = () => {
     };
     setLoading(true);
     axios
-      .put(`http://localhost:3001/books/${id}`, data)
+      .put(`https://book-store-mern-eta.vercel.app/books/${id}`, data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book edited successfully', { variant : 'success' });
