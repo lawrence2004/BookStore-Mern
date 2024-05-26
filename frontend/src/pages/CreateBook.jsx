97ce82a259b7fffd2f0ad5dd6533148478b99193
lawrence2004 <138093175+lawrence2004@users.noreply.github.com>
2024-05-26 16:39:29 +0530

    Update CreateBook.jsx

--- frontend/src/pages/CreateBook.jsx
+++ frontend/src/pages/CreateBook.jsx
@@ -22,7 +22,7 @@ const CreateBook = () => {
     };
     setLoading(true);
     axios
-      .post("http://localhost:3001/books", data)
+      .post("https://book-store-mern-eta.vercel.app/books", data)
       .then(() => {
         setLoading(false);
         enqueueSnackbar('Book created successfully', { variant : 'success'});
